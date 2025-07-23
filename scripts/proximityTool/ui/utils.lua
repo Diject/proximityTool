6993111ce5a184b8341c74349d2f998090bd1002
Diject <alexkozlovv@gmail.com>
2025-07-23 17:38:03 +0300

    add getTextHeight function

--- scripts/proximityTool/ui/utils.lua
+++ scripts/proximityTool/ui/utils.lua
@@ -28,4 +28,30 @@ function this.removeFromContent(content, index)
 end
 
 
+function this.getTextHeight(text, fontSize, width, mul)
+    if not mul then mul = 0.7 end
+    if #text == 0 then return 0 end
+    local words = {}
+    local charWidth = fontSize * mul
+    local rowMaxSize = math.floor(width / charWidth)
+    local rowCount = 1
+    for line in text:gmatch("[^\n]+") do
+        local currentRowSize = 0
+        for word in line:gmatch("%S+") do
+            local wordSize = utf8.len(word)
+            if currentRowSize + wordSize <= rowMaxSize then
+                currentRowSize = currentRowSize + wordSize + 1
+            else
+                rowCount = rowCount + 1
+                currentRowSize = wordSize ---@diagnostic disable-line: cast-local-type
+            end
+            table.insert(words, word)
+        end
+        rowCount = rowCount + 1
+    end
+    rowCount = rowCount - 1
+    return rowCount * fontSize, rowCount
+end
+
+
 return this
