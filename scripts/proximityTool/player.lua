8f56f7bfd264ea4e9ce7a62ac87bce6117cacae2
Diject <alexkozlovv@gmail.com>
2025-08-15 20:00:28 +0300

    cleanup

--- scripts/proximityTool/player.lua
+++ scripts/proximityTool/player.lua
@@ -543,15 +543,6 @@ return {
         getHUDMdata = getHUDMdata,
         setVisibility = setVisibility,
         setHUDMvisibility = setHUDMvisibility,
-        --TODO chage event sys
-        -- registerEvent = function (eventId, id, groupId, data)
-        --     local record = mapData.getRecord(id)
-        --     if not record then return end
-        --     if not record.events then record.events = {} end
-
-        --     record.events[eventId] = data
-        --     return true
-        -- end,
         removeRecord = function (recordId)
             return mapData.removeRecord(recordId)
         end,
